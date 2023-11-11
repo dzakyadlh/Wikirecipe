ef4e1a01f6284f41126bcea453f7235398a9cac8
dzakyadlh <106021129+dzakyadlh@users.noreply.github.com>
2023-11-11 21:05:42 +0700

    mandatory criteria done

--- app/src/main/java/com/dzakyadlh/wikirecipe/WikirecipeApp.kt
+++ app/src/main/java/com/dzakyadlh/wikirecipe/WikirecipeApp.kt
@@ -1,42 +1,72 @@
 package com.dzakyadlh.wikirecipe
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.dzakyadlh.wikirecipe.ui.navigation.Screen
+import com.dzakyadlh.wikirecipe.ui.screen.about.AboutScreen
 import com.dzakyadlh.wikirecipe.ui.screen.detail.DetailScreen
 import com.dzakyadlh.wikirecipe.ui.screen.home.HomeScreen
 import com.dzakyadlh.wikirecipe.ui.theme.WikirecipeTheme
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun WikirecipeApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
 ) {
-    NavHost(navController = navController, startDestination = Screen.Home.route) {
-        composable(Screen.Home.route) {
-            HomeScreen(navigateToDetail = { id ->
-                navController.navigate(
-                    Screen.Detail.createRoute(id)
-                )
-            })
-        }
-        composable(
-            route = Screen.Detail.route,
-            arguments = listOf(navArgument("id") { type = NavType.StringType })
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { TopAppBar(navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = modifier.padding(innerPadding)
         ) {
-            val id = it.arguments?.getString("id") ?: -1L
-            DetailScreen(id = id.toString(), navigateBack = { navController.navigateUp() })
-        }
-        composable(Screen.About.route) {
+            composable(Screen.Home.route) {
+                HomeScreen(navigateToDetail = { id ->
+                    navController.navigate(
+                        Screen.Detail.createRoute(id)
+                    )
+                })
+            }
+            composable(
+                route = Screen.Detail.route,
+                arguments = listOf(navArgument("id") { type = NavType.StringType })
+            ) {
+                val id = it.arguments?.getString("id") ?: -1L
+                DetailScreen(id = id.toString(), navigateBack = { navController.navigateUp() })
+            }
+            composable(Screen.About.route) {
+                AboutScreen()
+            }
         }
     }
 }
@@ -46,5 +76,62 @@ fun WikirecipeApp(
 fun WikirecipeAppPreview() {
     WikirecipeTheme {
         WikirecipeApp()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(navController: NavHostController, modifier: Modifier = Modifier) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    if (currentRoute == Screen.About.route) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    "Wikirecipe",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            navigationIcon = {
+                IconButton(onClick = { navController.navigateUp() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            },
+            actions = {
+                IconButton(onClick = { navController.navigate(Screen.About.route) }) {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "about_page"
+                    )
+                }
+            },
+            scrollBehavior = scrollBehavior,
+            modifier = modifier.background(color = MaterialTheme.colorScheme.primary)
+        )
+    } else if (currentRoute == Screen.Home.route) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    "Wikirecipe",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            actions = {
+                IconButton(onClick = { navController.navigate(Screen.About.route) }) {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "Localized description"
+                    )
+                }
+            },
+            scrollBehavior = scrollBehavior,
+            modifier = modifier.background(color = MaterialTheme.colorScheme.primary)
+        )
     }
 }
--- app/src/main/java/com/dzakyadlh/wikirecipe/ui/screen/about/AboutScreen.kt
+++ app/src/main/java/com/dzakyadlh/wikirecipe/ui/screen/about/AboutScreen.kt
@@ -1,2 +1,41 @@
 package com.dzakyadlh.wikirecipe.ui.screen.about
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.dzakyadlh.wikirecipe.R
+
+@Composable
+fun AboutScreen(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = modifier.fillMaxSize().padding(vertical = 40.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.avatar),
+            contentDescription = "creator",
+            modifier = modifier
+                .padding(4.dp)
+                .clip(
+                    CircleShape
+                )
+                .size(200.dp)
+        )
+        Text(text = "Dzaky Adla", Modifier.padding(vertical = 4.dp), style = MaterialTheme.typography.bodyLarge)
+        Text(text = "dzakyadlh@gmail.com", Modifier.padding(bottom = 4.dp), style = MaterialTheme.typography.bodyMedium)
+        Text(text = "Software Engineer", style = MaterialTheme.typography.bodyMedium)
+    }
+}
